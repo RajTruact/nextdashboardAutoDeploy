f1621016b58e2f505fa1fbf0e6fab535c6f5df89
RajTruact <raj@techpit.in>
2025-09-23 13:28:34 +0530

    c

--- src/app/(dashboard)/[role]/support/page.jsx
+++ src/app/(dashboard)/[role]/support/page.jsx
@@ -10,7 +10,7 @@ export default function Support() {
     <div>
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6 overflow-auto">
         <h3 className="mb-5 text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-7 capitalize">
-          Support {role}
+          Support Page {role}
         </h3>
         <div className="space-y-6">
           <SupportCard />
